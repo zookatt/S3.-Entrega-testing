0fd73d8461f72afb7d6f49df0e5c504391207ac9
Ekaterina Z <zotova.ea@gmail.com>
2023-12-14 12:38:08 +0100

    Exercise 2: Get the films of a certain director with .filter()

--- src/films.js
+++ src/films.js
@@ -8,8 +8,10 @@ function getAllDirectors(movies) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
+function getMoviesFromDirector(movies, director) {
+  let result = movies.filter((movie) => movie.director === director);
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
